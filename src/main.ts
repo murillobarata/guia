f6526a051aa9ef06820cda9e4f51b8558e984010
murillo barata <murillo.barata@dellead.com>
2019-12-27 13:28:09 -0300

    Add static files

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  // console.log(join(process.cwd() + '/../images/'));
+  app.use(express.static(join(process.cwd() + '/../static/')));
   await app.listen(3000);
 }
 bootstrap();
